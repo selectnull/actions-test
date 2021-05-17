0ca42b8c6449e5dfa15f85fe720de46ff2a42adc
Sasha Matijasic <sasha@selectnull.com>
2021-05-17 22:52:29 +0200

    Fix a bug.

--- src/test_app.py
+++ src/test_app.py
@@ -7,4 +7,4 @@ def test_index():
     response = client.get('/')
 
     assert response.status_code == 200
-    assert response.json().get('msg') == 'It works.'
+    assert response.json().get('msg') == 'It works great.'
